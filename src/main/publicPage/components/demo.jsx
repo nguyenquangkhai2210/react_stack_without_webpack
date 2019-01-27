deddcf603dac27af57996eeb119e8aad5288ecaa
unknown <nguyenquangkhai@gmail.com>
2019-01-27 14:58:01 +0700

    demo

---
+++ src/main/publicPage/components/demo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+class Demo extends Component {
+    render() {
+        return (
+            <div>Hello</div>
+        );
+    }
+}
+
+export default Demo;
